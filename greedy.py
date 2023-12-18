90e771f916a2dd6bbc819426b716a7058367885d
muhamad salman fauzi <salmanfauzi0512@gmail.com>
2023-12-19 01:12:12 +0700

    tugas project DAA - algoritma-greedy

--- greedy.py
+++ greedy.py
@@ -1,43 +1,62 @@
-# Python Code
-def greedy_algorithm(schedule):
-    # Initialize an empty schedule
-    result_schedule = []
+class GreedyScheduler:
+    def __init__(self):
+        self.jadwal = []
 
-    # For each time slot in the input schedule
-    for time_slot in schedule:
-        # Generate candidate rooms for the current time slot
-        candidates = generate_candidate_rooms()
+    def tambahMataKuliah(self, mataKuliah):
+        self.jadwal.append(mataKuliah)
 
-        # Select the best room from the candidates
-        selected_room = select_best_room(candidates)
+    def apakahRuanganTersedia(self, mataKuliah):
+        # Implementasi logika pengecekan ketersediaan ruangan
+        # Sesuaikan sesuai dengan aturan atau batasan spesifik Anda
+        # Pastikan juga memeriksa ketersediaan ruangan untuk kelas tertentu
+        return True  # Gantilah dengan logika sesuai kebutuhan
 
-        # Assign the selected room to the current time slot
-        time_slot['room'] = selected_room
+    def apakahWaktuTersedia(self, mataKuliah):
+        # Implementasi logika pengecekan ketersediaan waktu
+        # Sesuaikan sesuai dengan aturan atau batasan spesifik Anda
+        return True  # Gantilah dengan logika sesuai kebutuhan
 
-        # Add the updated time slot to the result schedule
-        result_schedule.append(time_slot)
+    def optimalkanJadwal(self):
+        self.jadwal.sort(key=lambda x: (x['waktuMulai'], x['kelas'], x['ruangan']))
 
-    return result_schedule
+        jadwal_optimalkan = []
 
-# Helper functions (placeholders, implement as needed)
-def generate_candidate_rooms():
-    # Implementation details here
-    pass
+        for mataKuliah in self.jadwal:
+            ruangan_tersedia = self.apakahRuanganTersedia(mataKuliah)
+            waktu_tersedia = self.apakahWaktuTersedia(mataKuliah)
 
-def select_best_room(candidates):
-    # Implementation details here
-    pass
+            if ruangan_tersedia and waktu_tersedia:
+                jadwal_optimalkan.append(mataKuliah)
 
-# Example usage
-initial_schedule = [
-    {'time': '9:00 AM', 'room': None},
-    {'time': '10:00 AM', 'room': None},
-    {'time': '11:00 AM', 'room': None},
-    # ... (other time slots)
-]
+        return jadwal_optimalkan
 
-final_schedule = greedy_algorithm(initial_schedule)
 
-# Print the final schedule
-for slot in final_schedule:
-    print(f"{slot['time']} - Room: {slot['room']}")
+# Contoh Penggunaan
+scheduler = GreedyScheduler()
+
+# Input pengguna untuk menambahkan mata kuliah
+jumlah_mata_kuliah = int(input("Masukkan jumlah mata kuliah: "))
+
+print("=================================================================")
+for i in range(1, jumlah_mata_kuliah + 1):
+    nama_mata_kuliah = input(f"Masukkan nama mata kuliah ke-{i}: ")
+    kelas = input(f"Masukkan kelas untuk {nama_mata_kuliah}: ")
+    ruangan = input(f"Masukkan ruangan untuk {nama_mata_kuliah}: ")
+    waktu_mulai = int(input(f"Masukkan waktu mulai untuk {nama_mata_kuliah} (contoh: 8 untuk pukul 8:00): "))
+    waktu_selesai = int(input(f"Masukkan waktu selesai untuk {nama_mata_kuliah} (contoh: 10 untuk pukul 10:00): "))
+    print("=================================================================")
+
+    # Tambahkan mata kuliah ke dalam jadwal
+    scheduler.tambahMataKuliah({'namaMataKuliah': nama_mata_kuliah, 'kelas': kelas, 'ruangan': ruangan, 'waktuMulai': waktu_mulai, 'waktuSelesai': waktu_selesai})
+
+# Optimalkan jadwal menggunakan Algoritma Greedy
+jadwal_optimalkan = scheduler.optimalkanJadwal()
+
+# Tampilkan jadwal yang sudah dioptimalkan dengan format yang lebih terstruktur
+print("Jadwal yang Dioptimalkan:")
+for mataKuliah in jadwal_optimalkan:
+    print(f"Mata Kuliah: {mataKuliah['namaMataKuliah']}")
+    print(f"Kelas: {mataKuliah['kelas']}")
+    print(f"Ruangan: {mataKuliah['ruangan']}")
+    print(f"Waktu Mulai: {mataKuliah['waktuMulai']}")
+    print(f"Waktu Selesai: {mataKuliah['waktuSelesai']}\n")
